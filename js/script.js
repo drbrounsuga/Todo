1bad335a406f2bcf47b1081517aa11693377f810
LaVaughn Haynes <drbrounsuga@gmail.com>
2015-11-01 09:40:19 -0600

    Final dev JS to modular production JS conversion update

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ var todoApp = (function($){
 	var $document,
 			$todo,
 			list = [],
+			progressStatus = ['open', 'started', 'complete'],
 			todoTemplate,
 
 			cacheDOM = function(){
@@ -39,12 +40,18 @@ var todoApp = (function($){
 			},
 			addListItem = function(e){
 				e.preventDefault();
-				var $userInput = $todo.find('input');
-				list.push({
-					text: $userInput.val(),
-					progress: 'open'
-				});
-				renderList();
+				var $userInput = $todo.find('input'),
+					inputValue = ($userInput.val()).trim();
+
+				if(inputValue){
+					//push new task to list
+					list.push({
+						text: inputValue,
+						progress: progressStatus[0]
+					});
+					renderList();
+				}
+
 				focusInput();
 			},
 			removeListItem = function(e){
@@ -54,6 +61,40 @@ var todoApp = (function($){
 				list.splice(index, 1);
 				renderList();
 			},
+			captureEnterPress = function(e){
+				if(e.keyCode === 13){
+					$todo.find('.add-todo').trigger('click');
+				}
+			},
+			cycleStatus = function(e){
+				e.preventDefault();
+
+				var statusFound = false,
+						newStatus = 0,
+						//get info about the list item clicked
+						$listElement = $(this).closest('li'),
+						currentClass = $listElement.attr('class'),
+						currentIndex = $listElement.data('index');
+
+				//find it's current status and shift it by 1
+				for(var i = 0, len = progressStatus.length; i < len && !statusFound; i++){
+					if(progressStatus[i] === currentClass){
+						statusFound = true;
+
+						/*
+						if going to next status doesn't exceed the progressStatus list length, 
+						go to next status. The default is to cycle to the first status */
+						if(i + 1 < len){
+							newStatus = i + 1;
+						}
+
+						//update the status
+						list[currentIndex].progress = progressStatus[newStatus];
+					}
+				}
+
+				renderList();
+			},
 			destroy = function(){
 				$todo.empty();
 				list = [];
@@ -61,7 +102,9 @@ var todoApp = (function($){
 				$todo.off('click', '.add-todo', addListItem);
 				$todo.off('click', '.delete', removeListItem);
 				$todo.off('keyup', 'input', checkInput);
+				$todo.off('keypress', 'input', captureEnterPress);
 				$todo.off('mouseenter', 'header', hoverSelect);
+				$todo.off('click', '.status', cycleStatus);
 			},
 			reset = function(){
 				destroy();
@@ -74,7 +117,9 @@ var todoApp = (function($){
 				$todo.on('click', '.add-todo', addListItem);
 				$todo.on('click', '.delete', removeListItem);
 				$todo.on('keyup', 'input', checkInput);
+				$todo.on('keypress', 'input', captureEnterPress);
 				$todo.on('mouseenter', 'header', hoverSelect);
+				$todo.on('click', '.status', cycleStatus);
 			};
 
 		init();
